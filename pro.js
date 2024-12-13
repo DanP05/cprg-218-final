2c33c59a33af9f890845ac46798d63180e6411a4
DanP05 <berti.penaguzman@edu.sait.ca>
2024-12-12 16:13:55 -0800

    Final details added
    all css, html, and java script details were added.

--- pro.js
+++ pro.js
@@ -6,3 +6,23 @@ setInterval(function () {
     img1.src = images[index]; 
     index = (index + 1) % images.length; 
 }, 2000);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const popup = document.querySelector('.popup');
+    const submitButton = document.querySelector('.form button[type="submit"]');
+    const okButton = document.querySelector('.popup button');
+    const form = document.querySelector('.form'); 
+
+    function showPopup(event) {
+        event.preventDefault(); 
+        popup.style.display = 'block';
+
+        const inputs = form.querySelectorAll('input, textarea');
+        inputs.forEach(input => input.value = ''); 
+    }
+    function hidePopup() {
+        popup.style.display = 'none'; 
+    }
+    submitButton.addEventListener('click', showPopup);
+    okButton.addEventListener('click', hidePopup);
+})
